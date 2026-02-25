778f46ab0be902ae9e309da42d5ad151bcb700af
Saichand Thokala <saichand1997.s5@gmail.com>
2026-02-25 19:53:08 +0530

    RepeatedCharaters

---
+++ src/RepeatedCharaters.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class RepeatedCharaters {
+
+    static void main(String[] args) {
+
+        String input = "JavaPrograming";
+
+        Map<Character, Long> result = input.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+
+        result.entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .forEach(System.out::println);
+
+    }
+}
