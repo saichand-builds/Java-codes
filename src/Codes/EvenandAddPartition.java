c667912fabcc8e22b99754632e5a13aec0c5f520
Saichand Thokala <saichand1997.s5@gmail.com>
2026-03-20 17:33:41 +0530

    Interview Codes

--- src/Codes/EvenandAddPartition.java
+++ src/Codes/EvenandAddPartition.java
@@ -11,8 +11,16 @@ public class EvenandAddPartition {
 
         List<Integer> list= Arrays.asList(1,2,3,4,5,6,7);
 
+/*
         Map<Boolean, List<Integer>> result=list.stream()
-                .collect(Collectors.partitioningBy(x->x%2==0));
+            .collect(Collectors.partitioningBy(x->x%2==0));
+*/
+
+
+        Map<String , List<Integer>> result=list.stream()
+                        .collect(Collectors.groupingBy(
+                                x->x % 2 == 0? "Even" :"Odd"
+                        ));
 
         System.out.println(result);
     }
