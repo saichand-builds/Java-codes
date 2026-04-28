1606ad39cc256f6d0f5d817f3a650793768091c8
Saichand Thokala <saichand1997.s5@gmail.com>
2026-04-28 11:59:55 +0530

    Interview Codes

---
+++ src/Iftrap1.java
@@ -0,0 +1,19 @@
+public class Iftrap1 {
+
+    static void main(String[] args) {
+
+
+    // pre increment
+
+    //  post increment
+
+        int x=5;
+        if(x++ > 5 ) (++x > 5 )
+        {
+            System.out.println("A");
+        }
+        else {
+            System.out.println("B");
+        }
+    }
+}
